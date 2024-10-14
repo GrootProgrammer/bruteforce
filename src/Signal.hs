7048ed7e84a5d7d25d8692af47cf7b4ebd6bcd35
r.t.groot@student.utwente.nl <r.t.groot@student.utwente.nl>
2024-10-14 15:10:00 +0200

    do easier version

--- src/Signal.hs
+++ src/Signal.hs
@@ -9,6 +9,7 @@
 {-# LANGUAGE UndecidableInstances #-}
 
 {-# LANGUAGE Unsafe #-}
+{-# OPTIONS_GHC -Wno-inline-rule-shadowing #-}
 
 module Signal
 where
@@ -16,16 +17,17 @@ where
 import Data.List
 
 
-data Signal a = End | a :- (Signal a)
+data Signal a = a :- (Signal a)
     deriving (Show, Eq)
 
+sRepeat :: a -> Signal a
+sRepeat i = i :- sRepeat i
+
 fromList :: [a] -> Signal a
-fromList (x : xs) = x :- (fromList xs)
-fromList [] = End
+fromList (x : xs) = x :- fromList xs
 
 toList :: Signal a -> [a]
 toList (x :- xs) = x : toList xs
-toList End = []
 
 now :: Signal a -> a
 now (h :- _) = h
@@ -37,13 +39,12 @@ register :: a -> Signal a -> Signal a
 register df input = df :- input
 
 mealy :: (s -> i -> (s, o)) -> s -> Signal i -> Signal o
-mealy f s End = End
-mealy f s (h :- t) = o' :- (mealy f s' t)
+mealy f s (h :- t) = o' :- mealy f s' t
     where
         (s', o') = f s h
 
 zipS :: (a -> b -> c) -> Signal a -> Signal b -> Signal c
-zipS f (hl:-tl) (hr:-tr) = (f hl hr) :- (zipS f tl tr)
+zipS f (hl:-tl) (hr:-tr) = f hl hr :- zipS f tl tr
 
 addS :: Signal Integer -> Signal Integer -> Signal Integer
 addS = zipS (+)
@@ -51,9 +52,12 @@ addS = zipS (+)
 subS :: Signal Integer -> Signal Integer -> Signal Integer
 subS = zipS (-)
 
-direct_integrator :: Signal Integer -> Signal Integer
-direct_integrator i = mealy (\(a1, a2) b -> ((b, a1), (a1 + a2))) (0,0) i
+directIntegrator :: Signal Integer -> Signal Integer
+directIntegrator = mealy (\(a1, a2) b -> ((b, a1), a1 + a2)) (0,0)
 
-{-# RULES 
-"morgans law" forall i. toList (register (0 :: Integer) (direct_integrator i)) = (toList (direct_integrator (register (0 :: Integer) i)))
+directIntegrator2 :: Signal Integer -> Signal Integer
+directIntegrator2 i = foldl addS (sRepeat 0) (take 4 $ iterate (register 0) i)
+
+{-# RULES
+"morgans law" forall i. register (0 :: Integer) (directIntegrator2 i) = directIntegrator2 (register (0 :: Integer) i)
 #-}
